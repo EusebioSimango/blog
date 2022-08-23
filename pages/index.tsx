698aedf4866038d12b71d42c1f2df5e391bd765e
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-23 02:14:08 +0200

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function App(){
+	return (
+
+		<div>
+			<Head>
+				<title>Eusébio Simango</title>
+				<meta name="description" content="A vida de um nerd adolescente em Moçambique" />
+			</Head>
+			<h1 className="text-black-500">Hello, world!</h1>
+		</div>    
+	)
+}
