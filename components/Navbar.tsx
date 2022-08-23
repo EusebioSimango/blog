67778f77549fe0885bf00bb84c02a08fce521d91
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-23 11:51:27 +0200

    Starting over

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Navbar(){
+  return (
+    <div>
+      <h1 className="text-red-500 font-">VENUS</h1>
+    </div>
+  )
+}
---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import React from 'react';
+
+
+export default function About(){
+  return (
+    <div>
+      <Head>
+        <title>Sobre</title>
+      </Head>
+      <h1 className='text-red-300'>About</h1>
+    </div>
+  );
+}
