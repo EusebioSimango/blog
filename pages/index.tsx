c358d1e69bbef7d5fc8452f461a68f5762364764
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-23 11:51:09 +0200

    Basics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function App(){
+
+export default function Home(){
 	return (
-
 		<div>
 			<Head>
 				<title>Eusébio Simango</title>
 				<meta name="description" content="A vida de um nerd adolescente em Moçambique" />
 			</Head>
-			<h1 className="text-black-500">Hello, world!</h1>
+			<h1 className="text-purple-500 bg-blue-200">Hello, world!</h1>
+			<Link href={"about"}>About</Link>
 		</div>    
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,11 @@ module.exports = {
     "./components/**/*.tsx"],
   theme: {
     extend: {
-      colors: {
-        brand: {
-          300: '#996DFF',
-          500: '#8257e6'
-        } 
-      },
       borderRadius: {
         md: '4px'
       }
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
   ],
 }
