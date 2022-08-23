f0a4911f12f19b22a29628a61e3634af3cfe9fb3
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-23 18:48:23 +0200

    Finished Social Link Section

---
+++ components/SocialLink.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+interface SocialLinkProps {
+	url: String,
+	icon: any
+}
+
+export default function SocialLink({ url, icon }: SocialLinkProps){
+
+	return (
+		<a href={url} target={"_blank"} className="">
+      <span className="text-gray-700 hover:text-gray-900 transition-all duration-700 cursor-pointer">
+        {icon}
+      </span>
+    </a>
+	)
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react';
+import { GithubLogo, TwitterLogo, LinkedinLogo, InstagramLogo, YoutubeLogo, GoogleLogo } from 'phosphor-react'
 import Navbar from './../components/Navbar'
+import SocialLink from './../components/SocialLink'
 import Avatar from './../images/me.png'
 
 export default function About(){
@@ -22,10 +24,18 @@ export default function About(){
 
             <h1 className="text-gray-900">Eusébio Simango</h1>
             <p className="text-gray-400">Web Developer</p>
+            <div className="flex flex-row flex-nowrap gap-3 mb-4">
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <GithubLogo className="w-12 h-12"/> }/>
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <TwitterLogo className="w-12 h-12"/> }/>
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <LinkedinLogo className="w-12 h-12"/> }/>
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <InstagramLogo className="w-12 h-12"/> }/>
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <YoutubeLogo className="w-12 h-12"/> }/>
+              <SocialLink  url="https://www.gitbub.com/EusebioSimango" icon={ <GoogleLogo className="w-12 h-12"/> }/>
+            </div>
           </div>
           <div className="md:mt-20 md:w-[calc(100vw-350px)]">
             <p className="text-gray-700 ">
-              Meu nome é <Link href={"https://github.com/EusebioSimango"} className="text-gray-1000">Eusébio Simango</Link>, e eu sou um adolescente moçambicano apaixonado por programação, tecnologia e ciência. Neste blog vou falar sobre a minha vida, dificuldades que encontrei na minha vida pessoal e profissional, toda minha jornada no mundo do desenvolvimeno de software.
+              Meu nome é <a href={"https://github.com/EusebioSimango"} className="bg-gray-1000">Eusébio Simango</a>, e eu sou um adolescente moçambicano apaixonado por programação, tecnologia e ciência. Neste blog vou falar sobre a minha vida, dificuldades que encontrei na minha vida pessoal e profissional, toda minha jornada no mundo do desenvolvimeno de software.
             </p>
           </div>
         </div>
