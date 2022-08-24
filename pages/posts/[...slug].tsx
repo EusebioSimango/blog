f848f57fdb2f63b662f4df0a685ea9cff979dd34
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-24 20:35:22 +0200

    Update [...slug].tsx

---
+++ pages/posts/[...slug].tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { gql } from '@apollo/client'
+import  client  from './../../apolloClient'
+
+export default function PostPage({post}){
+	console.log(post.postContent)
+	return (
+		<div>
+			<div>
+				{post.title}
+				<div dangerouslySetInnerHTML={{__html: post.postContent.html}} />
+			</div>
+		</div>
+	)
+}
+
+export async function getStaticPaths(){
+	const {data} = await client.query({
+		query: gql`
+			query {
+			  posts {
+			    slug
+			  }
+			}
+		`
+	})
+	const { posts } = data
+	const paths = posts.map( post => ({
+		params: {slug: [post.slug]}
+	}))
+	return {
+		paths, 
+		fallback: false
+	}
+}
+
+export async function getStaticProps({params}){
+	const slug = params.slug[0]
+	const {data} = await client.query({
+		query: gql`
+			query PostBySlug($slug: String!){ 
+			  posts (where: {slug : $slug}){
+			    title
+			    slug
+			    postDate
+			    coverImage {
+			      url
+			    }
+			    postContent {
+			      html
+			    }
+			  }
+			}
+		`,
+		variables: {slug}
+	})
+	const { posts } = data
+	const post = posts[0]
+	return {
+		props: {
+			post
+		}
+	}
+}
