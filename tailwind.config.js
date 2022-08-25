bba1e14addc562d892b18d103be69fd54a56df6c
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-25 15:39:10 +0200

    extending colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
     "./components/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        "blue": "#183152",
+        "link": "#FF3657",
+        "darkBg": "#0C141C",
+        "darkLink": "#51FCCC",
+        "cc": "#51FCCC"
+      },
       borderRadius: {
         md: '4px'
       }
+
     },
   },
   plugins: [
