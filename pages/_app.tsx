3bc3a031e33d4afca405c865a120bf0eb0145b57
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-25 02:18:53 +0200

    using ApolloProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import './../global.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apolloClient'
 
 export default function MyApp({ Component, pageProps }: AppProps){
   return (
-    <Component {...pageProps} />
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider> 
   )
 }
