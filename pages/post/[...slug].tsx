500bfaf051845f11fb6dca0b04f55e88201bf6fb
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-25 17:24:43 +0200

    Refactoring

--- pages/post/[...slug].tsx
+++ pages/post/[...slug].tsx
@@ -4,6 +4,8 @@ import  client  from '../../apolloClient'
 import Navbar from './../../components/Navbar'
 import { NextPage } from 'next'
 import { IPosts } from '..'
+import MetaData from '../../components/MetaData'
+
 
 export type PostType = {
 	title: string,
@@ -20,66 +22,78 @@ interface PostPageProps {
 	post: PostType
 }
 
-const  PostPage: NextPage<PostPageProps> = ({post}) => {
-	console.log(post.postContent)
-	return (
-		<div>
-            <Navbar />
-			<div className="pt-[100px] p-4">
-				{post.title}
-				<div className="postContent" dangerouslySetInnerHTML={{__html: post.postContent.html}} />
-			</div>
-		</div>
-	)
-}
+const GET_POSTS_BY_SLUG_QUERY = gql`
+	query {
+		posts {
+			slug
+		}
+	}
+`
+
+const GET_POST_BY_SLUG_QUERY = gql`
+	query PostBySlug($slug: String!){ 
+		posts (where: {slug : $slug}){
+			title
+			slug
+			postDate
+			coverImage {
+				url
+			}
+			postContent {
+				html
+			}
+		}
+	}
+`
 
 export async function getStaticPaths(){
 	const { data } = await client.query<IPosts>({
-		query: gql`
-			query {
-			  posts {
-			    slug
-			  }
-			}
-		`
+		query: GET_POSTS_BY_SLUG_QUERY
 	})
 	const { posts } = data
 	const paths = posts.map( post => ({
 		params: {slug: [post.slug]}
 	}))
+
 	return {
 		paths, 
 		fallback: false
 	}
 }
 
-export async function getStaticProps({params}){
+export async function getStaticProps({ params }){
 	const slug = params.slug[0]
 	const { data } = await client.query<IPosts>({
-		query: gql`
-			query PostBySlug($slug: String!){ 
-			  posts (where: {slug : $slug}){
-			    title
-			    slug
-			    postDate
-			    coverImage {
-			      url
-			    }
-			    postContent {
-			      html
-			    }
-			  }
-			}
-		`,
+		query: GET_POST_BY_SLUG_QUERY,
 		variables: { slug }
 	})
-	const { posts } = data
-	const post = posts[0]
+	const post = data.posts[0]
+
 	return {
 		props: {
 			post
 		}
 	}
+}
+
+const  PostPage: NextPage<PostPageProps> = ({post}) => {
+
+	return (
+		<div>
+			<MetaData metaData={{
+				title: `${ post.title }`,
+				description: `${ post.postContent }`,
+				author: 'Eusébio Simango',
+				keywords: ['adolscente', 'blog', 'eusebio simango', 'programador', 'tecnologia'],
+			}} />
+
+      <Navbar />
+			<div className="pt-[100px] p-4">
+				{post.title}
+				<div className="postContent" dangerouslySetInnerHTML={{__html: post.postContent.html}} />
+			</div>
+		</div>
+	)
 }
 
 export default PostPage
