e078f08b6192800ec7c252e221053ab3e31086f6
Eusebio Simango <eusebiosimango14@gmail.com>
2022-09-05 00:32:14 +0200

    UP

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Popover } from '@headlessui/react'
 import { List } from 'phosphor-react'
 import { useState, useEffect } from 'react'
 import Menu from './Menu'
@@ -13,19 +14,18 @@ export default function Navbar(){
         menu.classList.remove('active')
   }, [isMenuActive])
 
-  const toggleMenu = () => {
-    setIsMenuActive(!isMenuActive)
-  }
-
   console.log("Renderizou")
   return (
     <div className="fixed p-4 dark:text-white left-0 top-0 w-[100vw] dark:bg-darkBg bg-white text-center duration-500">
-      <List 
-        onClick={toggleMenu}
-        className='w-8 h-8 fixed top-[14px] left-2 cursor-pointer'
-      />
+      <Popover>
+        <Popover.Button className='fixed top-[14px] left-2'>
+          <List className='w-8 h-8'/>
+        </Popover.Button>
+        <Popover.Panel>
+          <Menu />
+        </Popover.Panel>
+      </Popover>
       <h1 className="text-4xl dark:text-darkLink text-link leading-tight">GINIUSS</h1>
-      <Menu />
     </div>
   )
 }
