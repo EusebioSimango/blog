e429561ac68075aa13ae23b8c1b015d234c5e8ce
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-11 21:48:09 +0200

    update

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -9,7 +9,7 @@ export default function SocialLink({ url, icon }: SocialLinkProps){
 		<a href={url} target={"_blank"} className="">
       <span className="text-gray-700 dark:text-white hover:text-gray-900 transition-all duration-700 cursor-pointer">
         {icon}
-      </span>
+      </span>df 
     </a>
 	)
 }
